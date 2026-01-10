2bc9d1b28e063ac162322f272b843c22c81ddb7a
TniRi <goodluckgame@mail.ru>
2026-01-10 21:11:43 +0300

    обработка ошибок

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,7 @@
 from services import UserService
 from flask import request, jsonify
 from models import UserSchema, SigninSchema
+from sqlalchemy.exc import IntegrityError
 
 
 class UserController:
@@ -9,17 +10,23 @@ class UserController:
         self.user_service = user_service
     
     def sign_up(self):
-        if request.is_json:
-            body = request.get_json()
-            user_data = UserSchema(**body)
-            self.user_service.sign_up(user_data)
-            return jsonify({"Message" : "Пользователь успешно зарегистрирован"}), 201
-        return jsonify({"Message" : "Не хватает данных!"}), 400
+        try:
+            if request.is_json:
+                body = request.get_json()
+                user_data = UserSchema(**body)
+                self.user_service.sign_up(user_data)
+                return jsonify({"Message" : "Пользователь успешно зарегистрирован"}), 201
+            return jsonify({"Message" : "Не хватает данных!"}), 400
+        except IntegrityError:
+            return jsonify({"Message" : "Такой пользователь уже существует!"}), 409
     
     def sign_in(self):
-        if request.is_json:
-            body = request.get_json()
-            user_data = SigninSchema(**body)
-            token = self.user_service.sign_in(user_data)
-            return jsonify({"token" : token}), 201
-        return jsonify({"Message" : "Не хватает данных!"}), 400
+        try:
+            if request.is_json:
+                body = request.get_json()
+                user_data = SigninSchema(**body)
+                token = self.user_service.sign_in(user_data)
+                return jsonify({"token" : token}), 201
+            return jsonify({"Message" : "Не хватает данных!"}), 400
+        except ValueError as e:
+            return jsonify({"Message" : str(e)}), 403
